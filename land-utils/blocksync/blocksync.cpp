9bc366e287b9556eaf4097e908ee7cdaee1edfe7
Alexey Shaforostov <kostelbeats@gmail.com>
2025-02-03 02:50:18 +0300

    blocksync fix

--- land-utils/blocksync/blocksync.cpp
+++ land-utils/blocksync/blocksync.cpp
@@ -2,24 +2,10 @@
 // Created by cv2 on 03.02.2025.
 //
 
-#include <iostream>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <cstring>
-#include <cstdlib>
-
-// Function to flush the file system buffers
-void sync_filesystem() {
-    if (sync() == -1) {
-        std::cerr << "Error: Unable to sync file system buffers: " << strerror(errno) << std::endl;
-        exit(1);
-    }
-    std::cout << "File system buffers synchronized successfully!" << std::endl;
-}
 
 int main() {
-    sync_filesystem();
+    // Function to flush the file system buffers
+    sync();
     return 0;
 }

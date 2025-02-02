46a36feb5d2d155e3d45e1a6730971bba62353bd
Alexey Shaforostov <kostelbeats@gmail.com>
2025-02-03 00:32:59 +0300

    add true and false

---
+++ land-utils/false/false.cpp
@@ -0,0 +1,10 @@
+//
+// Created by cv2 on 03.02.2025.
+//
+
+#include <cstdlib>
+// the false command
+int main() {
+    // complex software design
+    return EXIT_FAILURE;
+}
---
+++ land-utils/true/true.cpp
@@ -0,0 +1,9 @@
+//
+// Created by cv2 on 03.02.2025.
+//
+#include <cstdlib>
+// the true command
+int main() {
+    // complex software design
+    return EXIT_SUCCESS;
+}
